9fd04168c5885e900ade55e0a91da77781cb90b1
Shella <anandashella59@gmail.com>
2021-10-12 20:24:56 +0700

    first commit

---
+++ biodata.java
@@ -0,0 +1,21 @@
+public class biodata {
+    public static void main(String args []) {
+        String firstName = "Shella ";
+        String lastName = "Ananda";
+        String address = "Jalan Sekata Glugur";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "083164208560";
+        Boolean single = true;
+        String hobi = "Drakoran";
+        String makananFav = "Soto";
+        System.out.println("First name   : " + firstName + lastName);
+        System.out.println("Address      : " + address);
+        System.out.println("Age          : " + age + tahun);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Hobi         : " + hobi);
+        System.out.println("Single       : " + single);
+        System.out.println("Makanan Fav  : " + makananFav);
+
+    }
+}
